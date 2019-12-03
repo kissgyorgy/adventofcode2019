e20471cb8bc2272810e30dd55ffba7263f5eaba2
Kiss György <kissgyorgy@me.com>
2019-12-03 11:25:37 +0100

    solution: day2

---
+++ day2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type opCode int
+
+const (
+	add      opCode = 1
+	multiply opCode = 2
+	halt     opCode = 99
+)
+
+func runIntcode(program []int) {
+	var op opCode
+	var res, pos1, pos2, pos3 int
+
+	for i := 0; ; i += 4 {
+		op = opCode(program[i])
+		if op == halt {
+			return
+		}
+
+		switch pos1, pos2 = program[i+1], program[i+2]; op {
+		case add:
+			res = program[pos1] + program[pos2]
+		case multiply:
+			res = program[pos1] * program[pos2]
+		}
+
+		pos3 = program[i+3]
+		program[pos3] = res
+	}
+}
+
+func convertToProgram(content []byte) []int {
+	input := strings.Split(string(content), ",")
+	program := make([]int, len(input), len(input))
+	for i, v := range input {
+		num, _ := strconv.ParseInt(v, 10, 0)
+		program[i] = int(num)
+	}
+	return program
+}
+
+func main() {
+	content, _ := ioutil.ReadFile("day2-input.txt")
+	program := convertToProgram(content)
+	program[1] = 12
+	program[2] = 2
+	fmt.Println("IntCode Program:", program)
+
+	runIntcode(program)
+	fmt.Println("Program after running:", program)
+	fmt.Println("Result:", program[0])
+}
