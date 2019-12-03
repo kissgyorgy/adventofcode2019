0425629430628a5519d38b9ae29abe54b8cfe0d3
Kiss György <kissgyorgy@me.com>
2019-12-03 10:03:43 +0100

    solution: day1 part2

--- day1.go
+++ day1.go
@@ -9,20 +9,28 @@ import (
 )
 
 func calculateFuel(mass int) int {
-	return int(math.Floor(float64(mass/3))) - 2
+	fuelNeeded := int(math.Floor(float64(mass/3))) - 2
+	if fuelNeeded <= 0 {
+		return 0
+	}
+	fmt.Println("Need more fuel:", fuelNeeded)
+	return fuelNeeded + calculateFuel(fuelNeeded)
 }
 
 func main() {
 	f, _ := os.Open("day1-input.txt")
 	defer f.Close()
 
-	sum := 0
+	sum, totalFuelNeededForMass := 0, 0
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		massStr := scanner.Text()
 		mass, _ := strconv.ParseInt(massStr, 10, 64)
-		sum += calculateFuel(int(mass))
+		fmt.Println("Got mass:", mass)
+		totalFuelNeededForMass = calculateFuel(int(mass))
+		fmt.Println("Total fuel needed for mass:", totalFuelNeededForMass)
+		sum += totalFuelNeededForMass
 	}
 
 	fmt.Println("Summary:", sum)
