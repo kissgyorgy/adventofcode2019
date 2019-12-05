96ced6966030720f4dd7eeda80b005e94847ce40
Kiss György <kissgyorgy@me.com>
2019-12-05 22:05:44 +0100

    solution: day5 part2
    Put every opCode in a different branch (switch case) because parameters and
    instructions are too variadic, there is hardly any common parts. Also, address
    changes and argument numbers are closer to the actual operations, harder to make
    mistakes this way. param1 is always needed, safe to load that beforehand.

--- day5.go
+++ day5.go
@@ -9,14 +9,23 @@ import (
 	"strings"
 )
 
+const (
+	IntcodeFile = "day5-input.txt"
+	inputVal    = 5
+)
+
 type opCode int
 
 const (
-	add      opCode = 1
-	multiply opCode = 2
-	input    opCode = 3
-	output   opCode = 4
-	halt     opCode = 99
+	add         opCode = 1
+	multiply    opCode = 2
+	input       opCode = 3
+	output      opCode = 4
+	jumpIfTrue  opCode = 5
+	jumpIfFalse opCode = 6
+	lessThan    opCode = 7
+	equals      opCode = 8
+	halt        opCode = 99
 )
 
 type paramMode int
@@ -59,6 +68,7 @@ func getParam(memory []int, opAddr, nth int) int {
 		fmt.Println("Invalid parameter mode:", mode)
 		os.Exit(1)
 	}
+	fmt.Printf("param%d: %v (addr: %d)\n", nth, val, opAddr+nth)
 	return val
 }
 
@@ -67,7 +77,9 @@ func runIntcode(memory []int, inputVal int) {
 	var param1, param2, respos int
 
 	for addr := 0; ; {
-		fmt.Println("Instruction:", memory[addr])
+		fmt.Println("----")
+		fmt.Printf("Instruction: %v\n", memory[addr])
+		fmt.Println("Addr", addr)
 		op = opCode(memory[addr] % 100)
 		if op == halt {
 			return
@@ -75,37 +87,83 @@ func runIntcode(memory []int, inputVal int) {
 
 		param1 = getParam(memory, addr, 1)
 
-		if op == add || op == multiply {
+		switch op {
+		case add:
 			param2 = getParam(memory, addr, 2)
 			// Parameters that an instruction writes to will never be in immediate mode.
 			respos = memory[addr+3]
-		}
-
-		switch op {
-		case add:
 			fmt.Printf("ADD: %v+%v => %v\n", param1, param2, respos)
 			memory[respos] = param1 + param2
 			addr += 4
+
 		case multiply:
+			param2 = getParam(memory, addr, 2)
+			// Parameters that an instruction writes to will never be in immediate mode.
+			respos = memory[addr+3]
 			fmt.Printf("MUL: %v*%v => %v\n", param1, param2, respos)
 			memory[respos] = param1 * param2
 			addr += 4
+
 		case input:
 			respos = memory[addr+1]
 			fmt.Printf("INPUT: %v => %v\n", inputVal, respos)
 			memory[respos] = inputVal
 			addr += 2
+
 		case output:
 			fmt.Printf("OUT: %d \n", param1)
 			addr += 2
+
+		case jumpIfTrue:
+			if param1 != 0 {
+				param2 = getParam(memory, addr, 2)
+				fmt.Printf("JUMP: => %v\n", param2)
+				addr = param2
+			} else {
+				addr += 3
+			}
+
+		case jumpIfFalse:
+			if param1 == 0 {
+				param2 = getParam(memory, addr, 2)
+				fmt.Printf("JUMP: => %v\n", param2)
+				addr = param2
+			} else {
+				addr += 3
+			}
+
+		case lessThan:
+			param2 = getParam(memory, addr, 2)
+			fmt.Println("LESSTHAN:", param1, param2)
+			respos = memory[addr+3]
+			if param1 < param2 {
+				memory[respos] = 1
+			} else {
+				memory[respos] = 0
+			}
+			addr += 4
+
+		case equals:
+			param2 = getParam(memory, addr, 2)
+			fmt.Println("EQUALS:", param1, param2)
+			respos = memory[addr+3]
+			if param1 == param2 {
+				memory[respos] = 1
+			} else {
+				memory[respos] = 0
+			}
+			addr += 4
+
+		default:
+			fmt.Println("Invalid opcode:", op)
+			os.Exit(1)
 		}
 	}
 }
 
 func main() {
-	intcode := loadProgram("day5-input.txt")
+	intcode := loadProgram(IntcodeFile)
 	memory := initMemory(intcode)
 	fmt.Println("Memory:", memory)
-	inputVal := 1
 	runIntcode(memory, inputVal)
 }
