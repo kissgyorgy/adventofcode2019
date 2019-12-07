b9b8a9bb8a5a82d0e0263ce4b7d0019aab8b9556
Kiss György <kissgyorgy@me.com>
2019-12-07 16:05:31 +0100

    partial: day7 implemented permutation function

---
+++ day7/permutations.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+)
+
+func IterPermutations(numbers []int, r int) <-chan []int {
+	if r > len(numbers) {
+		err := errors.New("r cannot be bigger than the length of numbers")
+		panic(err)
+	}
+
+	ch := make(chan []int)
+	go func() {
+		defer close(ch)
+		permutate(ch, numbers, r)
+	}()
+	return ch
+}
+
+// an implementation similar to Python standard library itertools.permutations:
+// https://docs.python.org/3.8/library/itertools.html#itertools.permutations
+func permutate(ch chan []int, numbers []int, r int) {
+	n := len(numbers)
+
+	if r < 0 {
+		r = n
+	}
+
+	indices := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		indices[i] = i
+	}
+
+	cycles := make([]int, r, r)
+	for i := 0; i < r; i++ {
+		cycles[i] = n - i
+	}
+
+	nextPerm := func() []int {
+		perm := make([]int, r, r)
+		for i, ind := range indices[:r] {
+			perm[i] = numbers[ind]
+		}
+		return perm
+	}
+
+	ch <- nextPerm()
+
+	if n < 2 {
+		return
+	}
+
+	var tmp []int
+	var j int
+
+	for i := r - 1; i > -1; i-- {
+		cycles[i] -= 1
+		if cycles[i] == 0 {
+			tmp = append(indices[i+1:], indices[i])
+			indices = append(indices[:i], tmp...)
+			cycles[i] = n - i
+		} else {
+			j = len(indices) - cycles[i]
+			indices[i], indices[j] = indices[j], indices[i]
+			ch <- nextPerm()
+			i = r // start over the cycle
+			// i-- will apply, so i will be r-1 at the start of the next cycle
+		}
+	}
+}
---
+++ day7/permutations_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPermutations(t *testing.T) {
+	tests := []struct {
+		seq      []int
+		r        int
+		expected [][]int
+	}{
+		{
+			seq:      []int{},
+			r:        -1,
+			expected: [][]int{[]int{}},
+		},
+		{
+			seq:      []int{42},
+			r:        -1,
+			expected: [][]int{[]int{42}},
+		},
+		{
+			seq:      []int{42},
+			r:        1,
+			expected: [][]int{[]int{42}},
+		},
+		{
+			seq: []int{5, 6, 7},
+			r:   -1,
+			expected: [][]int{
+				[]int{5, 6, 7},
+				[]int{5, 7, 6},
+				[]int{6, 5, 7},
+				[]int{6, 7, 5},
+				[]int{7, 5, 6},
+				[]int{7, 6, 5},
+			},
+		},
+		{
+			seq: []int{5, 6, 7},
+			r:   3,
+			expected: [][]int{
+				[]int{5, 6, 7},
+				[]int{5, 7, 6},
+				[]int{6, 5, 7},
+				[]int{6, 7, 5},
+				[]int{7, 5, 6},
+				[]int{7, 6, 5},
+			},
+		},
+		{
+			seq: []int{5, 6, 7},
+			r:   2,
+			expected: [][]int{
+				[]int{5, 6},
+				[]int{5, 7},
+				[]int{6, 5},
+				[]int{6, 7},
+				[]int{7, 5},
+				[]int{7, 6},
+			},
+		},
+	}
+	for _, tt := range tests {
+		desc := fmt.Sprintf("%v,%d", tt.seq, tt.r)
+		t.Run(desc, func(t *testing.T) {
+			allPermutations := make([][]int, 0, len(tt.expected))
+			for perm := range IterPermutations(tt.seq, tt.r) {
+				allPermutations = append(allPermutations, perm)
+			}
+			assert.Equal(t, tt.expected, allPermutations)
+		})
+	}
+}
